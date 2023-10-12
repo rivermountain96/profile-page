dab64fecb3636de0d9f7b6bbcee24a0ed8941d9c
rivermountain96 <dlrkdtks7@gmail.com>
2023-10-12 18:26:13 +0900

    [ADD] svg 파일 추가

--- index.js
+++ index.js
@@ -16,27 +16,18 @@ $(document).ready(function () {
 
   const swiper = new Swiper(".swiper", {
     // Optional parameters
-    direction: "vertical",
+    direction: "horizontal",
     loop: true,
     autoplay: {
       delay: 3000,
       pauseOnMouseEnter: true,
     },
-
-    // If we need pagination
     pagination: {
       el: ".swiper-pagination",
     },
-
-    // Navigation arrows
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
-    },
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
     },
   });
 });
